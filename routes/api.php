610627bfb473e088901947b4bf7be2a9ab1c7547
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2026-05-31 11:55:07 +0600

    updated

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::middleware('auth:api')->prefix('/v1')->name('api.v1.')->group(base_path('routes/api/v1/protected.php'));
+Route::prefix('/v1')->name('api.v1.')->group(base_path('routes/api/v1/public.php'));
