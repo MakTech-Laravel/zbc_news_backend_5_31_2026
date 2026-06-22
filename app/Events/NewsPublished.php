a718617c4d5f82325525ee49accac4424d719723
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2026-06-22 15:48:39 +0600

    Change NewsPublished event to implement ShouldBroadcastNow for immediate broadcasting

--- app/Events/NewsPublished.php
+++ app/Events/NewsPublished.php
@@ -5,11 +5,12 @@ namespace App\Events;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class NewsPublished implements ShouldBroadcast
+class NewsPublished implements ShouldBroadcastNow  
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
